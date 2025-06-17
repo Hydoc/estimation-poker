0df2706afd7b75565b227215264ca1e0cf2d0187
sehnert <Felix.Sehnert@wolters-rundreisen.de>
2025-06-17 08:27:15 +0200

    Some stuff

--- backend/cmd/v2/handlers.go
+++ backend/cmd/v2/handlers.go
@@ -5,3 +5,27 @@ import "net/http"
 func (app *application) healthcheckHandler(writer http.ResponseWriter, request *http.Request) {
 	app.writeJSON(writer, http.StatusOK, envelope{"status": "ok"}, nil)
 }
+
+func (app *application) handleWS(writer http.ResponseWriter, request *http.Request) {
+	id, err := app.readIdParam(request)
+	if err != nil {
+		app.writeJSON(writer, http.StatusBadRequest, envelope{"error": err.Error()}, nil)
+		return
+	}
+	app.logger.Info(id)
+	return
+}
+
+func (app *application) tablesHandler(writer http.ResponseWriter, request *http.Request) {
+	var message struct {
+		Name    string
+		Payload any
+	}
+
+	err := app.readJSON(writer, request, &message)
+	if err != nil {
+		app.writeJSON(writer, http.StatusBadRequest, envelope{"error": err.Error()}, nil)
+		return
+	}
+	return
+}
--- backend/cmd/v2/routes.go
+++ backend/cmd/v2/routes.go
@@ -10,6 +10,8 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/api/v2/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/api/v2/tables", app.tablesHandler)
+	router.HandlerFunc(http.MethodPost, "/api/v2/tables/{id}/connect", app.handleWS)
 
 	return router
 }
