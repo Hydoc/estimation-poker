26259755b3f3cf475e93228560165a6fed8eb616
Felix Sehnert <felixsehnert@yahoo.de>
2025-07-05 10:20:08 +0200

    - added forgotten t.helper

--- backend/internal/assert/assert.go
+++ backend/internal/assert/assert.go
@@ -15,12 +15,16 @@ func Equal[T comparable](t *testing.T, got, want T) {
 }
 
 func True(t *testing.T, got bool) {
+	t.Helper()
+
 	if !got {
 		t.Errorf("got: %v, want: true", got)
 	}
 }
 
 func False(t *testing.T, got bool) {
+	t.Helper()
+
 	if got {
 		t.Errorf("got: %v, want: false", got)
 	}
