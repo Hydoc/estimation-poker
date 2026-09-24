cc224dd268bd351b0efe8a5422b9cec9beed059a
Felix Sehnert <felixsehnert@yahoo.de>
2026-09-24 19:52:16 +0200

    WIP - new Home View for easier testing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import { useRoom } from "@/composables/useRoom.ts";
+import HomeV2View from "@/views/HomeV2View.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +18,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/RoomView.vue"),
     },
+    {
+      path: "/home",
+      name: "homeV2",
+      component: HomeV2View,
+    }
   ],
 });
 
---
+++ frontend/src/views/HomeV2View.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const ws = new WebSocket();
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
